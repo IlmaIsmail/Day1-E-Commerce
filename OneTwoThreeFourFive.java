4dc22c010871af957f61427ae2c3a25d86ded261
Ilma Ismail <139671858+IlmaIsmail@users.noreply.github.com>
2025-03-21 00:20:42 +0000

    Reversing the left and right numbers

--- OneTwoThreeFourFive.java
+++ OneTwoThreeFourFive.java
@@ -31,7 +31,37 @@ public class OneTwoThreeFourFive{
         }
       }
 
+         /* 5)Reverse the left and right numbers like
+         You must use only a loop
+            input  9
+            output 4 3 2 1 5 9 8 7 6
 
+            input 13
+            output 6 5 4 3 2 1 7 13 12 11 10 9 8 */
+        
+      System.out.println("--------------------------------------------------");
+      Scanner scanner = new Scanner(System.in);
+      System.out.print("Enter a Number: ");
+      int number = scanner.nextInt();
+      int middle = number/2;
+      for (int i = 1; i <= number; i++ )
+       {
+        if(i <= middle)
+        {
+          System.out.print((middle-i+1)+" ");  
+        }
+        else
+        {
+           if(i == middle + 1)  
+           {
+              System.out.print(i+" ");  
+           }
+           else
+           {
+              System.out.print((number - i+ middle + 2) +" "); 
+           }
+        }
+       }
 
 
 
