5d4140ab16a04478abe6d7a15960daf0c3527bcf
Ilma Ismail <139671858+IlmaIsmail@users.noreply.github.com>
2025-03-21 00:31:10 +0000

    Editing Readme

---
+++ Student.java
@@ -0,0 +1,34 @@
+/* 6) Create a student class
+	-create 5 student objects and store it in 
+	a proper datastructure
+    - find the average marks of the students */
+    
+import java.util.*;
+
+public class Student{
+    private String name;
+    private int marks;
+
+    public Student(String name,int marks){
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getMarks(){
+        return this.marks;
+    }
+
+    public void setMarks(int marks){
+        this.marks=marks;
+    }
+
+   
+}
