02ddc5b913ec5ecbb41d7dd92e444fbda034189c
Ilma Ismail <139671858+IlmaIsmail@users.noreply.github.com>
2025-03-21 00:41:03 +0000

    E-Book and Audio Book

---
+++ AudioBook.java
@@ -0,0 +1,14 @@
+
+class AudioBook extends Book {
+    private double duration;
+
+    public AudioBook(String title, String author, double duration) {
+        super(title, author);
+        this.duration = duration;
+    }
+
+    @Override
+    public String getInfo() {
+        return super.getInfo() + ", Duration: " + duration + " hours";
+    }
+}
