226b7ff66cbb5dd3f2191d0dd59d877d06e9b62c
Ilma Ismail <139671858+IlmaIsmail@users.noreply.github.com>
2025-03-21 00:19:42 +0000

    printing the odd numbers within a range

--- OneTwoThreeFourFive.java
+++ OneTwoThreeFourFive.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.util.Scanner;
 
-public class one{
+public class OneTwoThreeFourFive{
     public static void main(String[] args)
     {
       // 1)Print a mesage
@@ -21,6 +21,15 @@ public class one{
         System.out.println(i);
       }
 
+         // 4)print the odd numbers between 1 to 20
+      System.out.println("--------------------------------------------------");
+      for (int i = 1; i <= 20; i++)
+      {
+        if(i % 2 == 1)
+        {
+           System.out.println(i); 
+        }
+      }
 
 
 
@@ -28,7 +37,6 @@ public class one{
 
 
 
-      
     }
 
 }
